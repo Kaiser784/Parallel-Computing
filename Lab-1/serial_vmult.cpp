02347acf2955e53ca1de21bcb2c827a23274fc82
unknown <ced19i002@iiitdm.ac.in>
2022-08-10 10:31:29 +0530

    Lab-1 Codes

---
+++ Lab-1/serial_vmult.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <time.h>
+
+#define SIZE 100000
+
+int main()
+{
+    double a[SIZE], b[SIZE], c[SIZE], rand_a, rand_b;
+    double start, end, exec;
+
+    start = omp_get_wtime();
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        rand_a = rand();
+        rand_b = rand();
+
+        a[i] = i*rand_a;
+        b[i] = i*rand_b;
+        
+        for(int j = 1; j < SIZE; j++)
+        c[i] = a[i] * b[i];
+    }
+
+    end = omp_get_wtime();
+
+    exec = end - start;
+
+    printf("Serial Exec time - %f\n", exec);
+
+    return 0;
+}
